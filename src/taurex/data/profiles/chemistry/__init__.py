9db9ef7372ef8109174c9d6f860a7d66b29b91e3
Sushuang Ma <85495927+SushuangMa@users.noreply.github.com>
2025-03-04 15:57:27 +0000

    Custom Gas Class added

--- src/taurex/data/profiles/chemistry/__init__.py
+++ src/taurex/data/profiles/chemistry/__init__.py
@@ -6,5 +6,7 @@ from .taurexchemistry import TaurexChemistry
 from .gas.constantgas import ConstantGas
 from .gas.twolayergas import TwoLayerGas
 from .gas.powergas import PowerGas
+from .gas.customgas import CustomGas
+from .gas.twopointgas import TwoPointGas
 from .filechemistry import ChemistryFile
 from .autochemistry import AutoChemistry
